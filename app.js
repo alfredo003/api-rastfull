e8efca26a5632e89d5be873df5869c18f9ea3c13
alfredo003 <59560615+alfredo003@users.noreply.github.com>
2022-11-13 04:17:34 +0100

    refactory

--- app.js
+++ app.js
@@ -3,9 +3,7 @@ const { randomUUID } = require('crypto');
 const fs = require("fs")
 const app = express();
 const PORT = 2000;
-
 app.use(express.json())
- 
 let users = [];
 
 fs.readFile("./database/data.json","utf-8",(err,data)=>{
@@ -16,9 +14,17 @@ fs.readFile("./database/data.json","utf-8",(err,data)=>{
   }
 });
 
-// create new user
-app.post("/user/created",(req,res)=>{
+app.get("/users",(req,res)=>{
+  res.json(users); 
+})
 
+app.get("/users/:id",(req,res)=>{
+  const {id} = req.params;
+  const user = users.find(user => user.id == id);
+  res.json(user)
+})
+
+app.post("/users",(req,res)=>{
   const {name,birthYear,gander} = req.body;
   const id = randomUUID();
   const user = {
@@ -27,38 +33,12 @@ app.post("/user/created",(req,res)=>{
     birthYear,
     gander
   };
-
   users.push(user);
-  
-  fs.writeFile('./database/data.json',JSON.stringify(users),(err)=>{
-    if(err){
-        console.log(err);
-    }else{
-        console.log("Product created with success!...")
-    }    
-  });
-
+  writeFile(users);
   res.json({message:"User created with success!",user:name})
-
 })
 
-//view all users
-app.get("/users",(req,res)=>{
-  res.json(users); 
-})
-
-// view a user using reference by parametre {:id}
-app.get("/users/:id",(req,res)=>{
-
-  const {id} = req.params;
-  const user = data.find(user => user.id == id);
-
-res.json(user)
-})
-
-// update data of user
-app.put("/user/update/:id",(req,res)=>{
-
+app.put("/users/:id",(req,res)=>{
   const {id} = req.params;
   const {name,birthYear,age}= req.body;
   const userId =  users.findIndex(user => user.id == id);
@@ -68,38 +48,25 @@ app.put("/user/update/:id",(req,res)=>{
   birthYear,
   age
   }
+  writeFile(users);
+  res.json({message:"User updated with success"})
+})
 
+app.delete('/users/:id',(req,res)=>{
+  const {id} = req.params;
+  const userIndex = users.findIndex(user => user.id == id);
+  users.splice(userIndex,1);
+  writeFile(users);
+  res.json({message:"User deleted with success"})
+})
+
+function writeFile(users){
   fs.writeFile('./database/data.json',JSON.stringify(users),(err)=>{
     if(err){
       console.log(err)
     }else{
-      console.log('Update with success!')
+      console.log('success!')
     }
   })
-  
-  
-  res.json({message:"User updated with success"})
-})
-
-// Delete user
-app.delete('/user/delete/:id',(req,res)=>{
-
-  const {id} = req.params;
-
- 
-  const userIndex = users.findIndex(user => user.id == id);
-   users.splice(userIndex,1);
-   
-   fs.writeFile('./database/data.json',JSON.stringify(users),(err)=>{
-    if(err){
-      console.log(err)
-    }else{
-      console.log('Update with success!')
-    }
-  })
-  
-})
-
-
-
+}
 app.listen(PORT,()=>console.log('SERVER RUNNING...! PORT:',PORT))
